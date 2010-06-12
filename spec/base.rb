3cb64f419fee32bc1519e3eea81006475d765748
Aleksey Palazhchenko <alek.silverstone@gmail.com>
2010-06-13 00:38:32 +0400

    Fix error in spec.
    It's Sass, not Haml!

--- spec/base.rb
+++ spec/base.rb
@@ -11,7 +11,7 @@ def write_sass(location, css_file = "screen")
   sass_path = File.join(location, "#{css_file}.sass")
   File.open(sass_path, "w") do |f|
     f.write <<EOF
-%h1
+h1
   font-size: 42em
 EOF
   end
